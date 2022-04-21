a66633f6241673f6dccd8372f66065b6f8df570e
cybille <rebeccacybille@gmail.com>
2022-04-21 18:09:45 -0400

    g

--- HousingAnalysis.py
+++ HousingAnalysis.py
@@ -1,2 +1,29 @@
+
 import kivy
-import kivyMD
+import kivymd
+from kivymd.app import MDApp
+from kivy.app import App
+from AllTemplates import AllTemplates
+from AllScreens import AllScreens
+  
+#Load all screens and layouts
+AllScreens()
+
+
+# loads all templates
+AllTemplates().loadTemplates()
+
+# load screen manager .kv file
+class Fre_Hotels():
+    pass
+
+class Fre_Hotels_Run(MDApp):
+    def build(self):
+        self.title = "FRE Hotel"
+        self.theme_cls.theme_style = "Light"
+        self.theme_cls.primary_palette = "BlueGray"
+        return AllScreens.create(self)
+
+#runs application
+
+Fre_Hotels_Run().run()
